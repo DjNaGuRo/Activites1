1eb6c44f7aaedcbf7df0e96f26d2d9791149fc47
DjNaGuRo <guyrostan_nana@yahoo.fr>
2017-08-29 01:52:04 +0100

    Calcul du nombres de pages de 10 messages et création d'un formulaire pour parcourir ces pages

--- minichat.php
+++ minichat.php
@@ -78,6 +78,40 @@ catch(Exception $e)
 
 }
 
+$nbre_msg = $bdd->query('SELECT count(id) AS nb_msg FROM minichat'); // On récupère le nombre de messages dans la table minichat de la BDD
+$nbre_msg = $nbre_msg->fetch();
+$nbre_page = $nbre_msg['nb_msg'] / 10;
+if($nbre_msg % 10 != 0)
+    $nbre_page += 1;
+?>
+
+<!--Formulaire permettant de choisir la page d'anciens messages à afficher -->
+<form method="post" action="minichat.php">
+    <?php
+echo "Pages : ";
+if(isset($_POST['page']))
+{
+    $limit = ($_POST['page'] - 1)* 10;
+    for ($i=1; $i < $nbre_page; $i++) { 
+    if($_POST['page'] == $i)
+        echo '<input type="radio" name="page" value="'.$i.'" id="page'.$i.'" checked="checked"/><label for="page'.$i.'">page '.$i.' </label>';
+    else
+        echo '<input type="radio" name="page" value="'.$i .'" id="page'.$i.'" /><label for="page'.$i.'">page '.$i.' </label>';
+    }
+}
+else
+{
+    $limit = 0;
+    for ($i=1; $i < $nbre_page; $i++) { 
+    if($i == 1)
+        echo '<input type="radio" name="page" value="1" id="page1" checked="checked"/><label for="page1">page 1 </label>';
+    else
+        echo '<input type="radio" name="page" value="'.$i .'" id="page'.$i.'" /><label for="page'.$i.'">page '.$i.' </label>';
+    }
+}    
+?>
+<input type="submit" name="Afficher" value="Afficher">
+</form>
 
 // Récupération des 10 derniers messages
 
